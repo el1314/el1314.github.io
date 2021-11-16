7ed4055927b863c23b0e882188907a4130c5a36c
el1314 <eternallove1314@outlook.com>
2021-11-17 00:06:56 +0800

    initial commit

---
+++ layouts/page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-main">
+    <nuxt />
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Navigation from '../components/Navigation';
+import Footer from '../components/Footer';
+
+export default {
+  components: {
+    Navigation,
+    Footer
+  }
+}
+</script>
