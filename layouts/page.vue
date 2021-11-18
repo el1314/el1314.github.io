b09856ad4bfebbc32a68b066b517f0fa1f0868de
Jane <72009576+el1314@users.noreply.github.com>
2021-11-18 15:03:39 +0800

    Update page.vue

--- layouts/page.vue
+++ layouts/page.vue
@@ -2,15 +2,18 @@
   <div class="container-main">
     <nuxt />
     <Footer></Footer>
+    <ForkOnGithub></ForkOnGithub>
   </div>
 </template>
 
 <script>
 import Footer from '../components/Footer';
+import ForkOnGithub from '../components/Fork-On-Github';
 
 export default {
   components: {
-    Footer
+    Footer,
+    ForkOnGithub
   }
 }
 </script>
