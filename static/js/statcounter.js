9341d49d1cd5534ce0a9075b082dba948fe2fd78
el1314 <hzn2017@outlook.com>
2022-01-30 00:12:11 +0800

    asdf

--- static/js/statcounter.js
+++ static/js/statcounter.js
@@ -1,9 +1,11 @@
-if (window.location.host == "moonhea.com") {
-	window.sc_project=12707204; 
-	window.sc_invisible=1; 
-	window.sc_security="c59e8848"; 
-	window.scJsHost = "https://";
-	var s = document.createElement("script");
-	s.src = "https://statcounter.com/counter/counter.js";
-	document.body.appendChild(s);
-}
+setTimeout(function() {
+	if (window.location.host == "moonhea.com") {
+		window.sc_project=12707204; 
+		window.sc_invisible=1; 
+		window.sc_security="c59e8848"; 
+		window.scJsHost = "https://";
+		var s = document.createElement("script");
+		s.src = "https://statcounter.com/counter/counter.js";
+		document.body.appendChild(s);
+	}
+},500);
