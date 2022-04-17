99a84b02f67294b109f55922ebaec5afa9311108
Jane <72009576+el1314@users.noreply.github.com>
2022-04-17 21:22:51 +0800

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,23 @@
 <template>
-	<div class="blog-wrap">
-		<main class="blog-main">
-			<p>404 not found.</p>
-			<p>Go back to <a href="/" class="err-home">Home</a></p>
-		</main>
-	</div>
+  <div class="main-content">
+    <div class="blog-wrap">
+      <main class="blog-main">
+        <DeskBanner deskOwner="Moonhea"></DeskBanner>
+        <p>
+          不想看碎碎念？来看看我的<a href="https://moonhea.com/blog"><strong>博客<i class="fa fa-external-link"></i></strong></a>吧！
+        </p>
+        <p>404 not found.</p>
+			  <p>Go back to <a href="/" class="err-home">Home</a></p>
+      </main>
+    </div>
+  </div>   
 </template>
+
+<script>
+import DeskBanner from "~/components/Desk-Banner";
+export default {
+  components: {
+    DeskBanner
+  },
+};
+</script>
